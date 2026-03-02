fb88b3060aced10a7c63a9c1a66a72e3b7418040
Jaiyam Sharma <jaiyamsharma@gmail.com>
2026-03-03 03:18:42 +0900

    feat: allow engine to set prompt at runtime (#15)
    * feat: export onnx with dynamic prompt
    
    * feat: set engine prompt at runtime
    
    * chore: very simple example of person segmentation

--- python/onnxexport.py
+++ python/onnxexport.py
@@ -12,7 +12,7 @@ processor = Sam3Processor.from_pretrained("facebook/sam3")
 
 model.eval()
 
-prompt="dog"
+prompt="person"
 
 # 2. Build a sample batch (same as your example)
 image_url = "http://images.cocodataset.org/val2017/000000077595.jpg"
@@ -23,39 +23,39 @@ inputs = processor(images=image, text=prompt, return_tensors="pt").to(device)
 pixel_values = inputs["pixel_values"]
 input_ids = inputs["input_ids"]
 attention_mask = inputs["attention_mask"]
-print(input_ids.shape)
+
+print("input_ids", input_ids.shape, input_ids.dtype)
+print(input_ids)
+print()
+print("attention_mask", attention_mask.shape, attention_mask.dtype)
+print(attention_mask)
 
 # 3. Wrap Sam3Model so the ONNX graph has clean inputs/outputs
 class Sam3ONNXWrapper(torch.nn.Module):
-    def __init__(self, sam3, input_ids, attention_mask):
+    def __init__(self, sam3):
         super().__init__()
         self.sam3 = sam3
-        self.register_buffer("const_input_ids", input_ids.to(torch.int64).cpu())
-        self.register_buffer("const_attention_mask", attention_mask.to(torch.int64).cpu())
 
-    def forward(self, pixel_values):
+    def forward(self, pixel_values, input_ids, attention_mask):
         outputs = self.sam3(
             pixel_values=pixel_values,
-            input_ids=self.const_input_ids,
-            attention_mask=self.const_attention_mask,
-        )
-        # Typical useful outputs
-        instance_masks = torch.sigmoid(outputs.pred_masks)  # [B, Q, H, W]
-        semantic_seg = outputs.semantic_seg                 # [B, 1, H, W]
-        return outputs.pred_masks, outputs.semantic_seg # instance_masks, semantic_seg
+            input_ids=input_ids,
+            attention_mask=attention_mask)
+        
+        return outputs.pred_masks, outputs.semantic_seg
 
-wrapper = Sam3ONNXWrapper(model, input_ids, attention_mask).to(device).eval()
+wrapper = Sam3ONNXWrapper(model).to(device).eval()
 
 # 5. Export to ONNX
 output_dir = Path(f"onnx_weights")
 output_dir.mkdir(exist_ok=True)
-onnx_path = str(output_dir / f"sam3_static.onnx")
+onnx_path = str(output_dir / f"sam3_dynamic.onnx")
 
 torch.onnx.export(
     wrapper,
-    (pixel_values),
+    (pixel_values, input_ids, attention_mask),
     onnx_path,
-    input_names=["pixel_values"],
+    input_names=["pixel_values", "input_ids", "attention_mask"],
     output_names=["instance_masks", "semantic_seg"],
     dynamo=False,
     opset_version=17,
